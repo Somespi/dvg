10c3269cd62b51a76b45ecb5e6e067a6226b5d95
Someone <120342697+Somespi@users.noreply.github.com>
2023-09-18 19:30:37 +0400

    add luma for grayscale

--- src/dvg.rs
+++ src/dvg.rs
@@ -1,8 +1,10 @@
-use std::{fs::File, io::Write, error::Error};
+use std::{fs::File, io::Write};
+
+use image::Luma;
 
 
 pub struct DvgImage {
-    data: Vec<u8>,
+    data: Vec<Luma<u8>>,
     width: u32,
     height: u32
 }
@@ -10,25 +12,23 @@ pub struct DvgImage {
 impl DvgImage {
     pub fn new(width: u32, height: u32) -> DvgImage {
         DvgImage {
-            data: vec![0u8; (width  * height ) as usize],
+            data: vec![Luma([0u8]); (width * height) as usize],
             width,
             height
         }
     }
 
-    pub fn get_pixel(&self, x: u32, y: u32) -> u8 {
-        self.data[(y * self.width + x) as usize]
+    pub fn put_pixel(&mut self, x: &u32, y: &u32, pixel: Luma<u8>) {
+        self.data[(x + y * self.width) as usize] = pixel;
     }
 
-    pub fn set_pixel(&mut self, x: u32, y: u32, pixel: u8) {
-        self.data[(y * self.width + x) as usize] = pixel
-    }
 
-    pub fn save(self, path: &str)  {
+    pub fn save(&self, path: &str)  {
         let mut file = File::create(path).unwrap();
         file.write_all(&self.width.to_le_bytes()).unwrap();
         file.write_all(&self.height.to_le_bytes()).unwrap();
-        file.write_all(&self.data).unwrap();
-        Ok::<(),()>(());
+        let data = &self.data.iter().map(|pixel| pixel[0]).collect::<Vec<u8>>();
+        file.write_all(&data).unwrap();
+        let _ = Ok::<(),()>(());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use std::fs::metadata;
+
+use image::Luma;
 mod dvg;
 
 fn main() {
     let width = 800;
     let height = 600;
-    let dvg_image = dvg::DvgImage::new(width, height);
+    let mut dvg_image = dvg::DvgImage::new(width, height);
 
-    dvg_image.save("dvg_image.dvg");
-    
+    for x in 0..width {
+        for y in 0..height {
+            let intensity = ((x + y) * 2) as u8;
+            dvg_image.put_pixel(&x, &y, Luma([intensity]));
+
+        }
+    }
+
+    dvg_image.save("dvg_image2.dvg");
 
 }
